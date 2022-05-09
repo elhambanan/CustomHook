81a4c4555ed83e7a59c34c9e10e0e308acc606e3
elhambanan <elham.banagozar@gmail.com>
2022-05-09 15:22:26 +0430

    create useFetch() hook

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import useFetch from './useFetch';
 
 function App() {
-  const {loading, error, data} = useFetch("https://jsonplaceholder.typicode.com/users")
+  const {loading, error, data} = useFetch(
+    "https://jsonplaceholder.typicode.com/users")
 
   return (
     <div className="App">
